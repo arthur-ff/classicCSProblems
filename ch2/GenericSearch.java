cdda09820d0491f31181e6cbe7c91b15574edd22
Arthur Fakhertdinov <a.fahertdinov@omp.ru>
2022-11-05 21:30:26 +0300

    Generic Search class

---
+++ ch2/GenericSearch.java
@@ -0,0 +1,71 @@
+package ch2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class GenericSearch {
+
+    public static <T extends Comparable<T>> boolean linearContains(List<T> list, T key) {
+        for (T item : list) {
+            if (item.compareTo(key) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <T extends Comparable<T>> boolean binaryContains(List<T> list, T key) {
+        int low = 0;
+        int high = list.size() - 1;
+        while (low <= high) {
+            int middle = (low + high)/2;
+            int comparsion = list.get(middle).compareTo(key);
+            if (comparsion < 0 ) {
+                low = middle + 1;
+            }
+            else if(comparsion > 0) {
+                high = middle - 1;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static class Node<T> implements Comparable<Node<T>> {
+        final T state;
+        Node<T> parent;
+        double cost;
+        double heuristic;
+
+        Node(T state, Node<T> parent) {
+            this.state = state;
+            this.parent = parent;
+        }
+
+        Node(T state, Node<T> parent, double cost, double heuristic) {
+            this.state = state;
+            this.parent = parent;
+            this.cost = cost;
+            this.heuristic = heuristic;
+        }
+
+        @Override
+        public int compareTo(Node<T> other) {
+            Double mine = cost + heuristic;
+            Double theirs = other.cost + other.heuristic;
+            return mine.compareTo(theirs);
+        }
+    }
+    public static <T> Node
+}
